28deda2d24f2edf5e6a6f666c8e578e8ee019849
PeipengWang <wangpeipeng123@163.com>
2026-05-13 09:10:42 +0800

    create repo

---
+++ src/main/java/com/example/evaluationalgorithm/EvaluationAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.example.evaluationalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaluationAlgorithmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/evaluationalgorithm/EvaluationAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evaluationalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
